7c013f1ed9f3a6bf3e38e3a8255ff0faf19c89af
Calvin Chu <cchu00@stuy.edu>
2019-03-15 11:49:08 -0400

    classwork

---
+++ Merge.java
@@ -0,0 +1,20 @@
+public class Merge {
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[] data){
+    msH(data, data, 0, data.length);
+  }
+  public static void msH(int[] data, int temp[], int lo, int hi) {
+    if (hi > lo) {
+      msH(data, data.subarray(lo, (hi - lo) / 2), lo, (hi - lo) / 2);
+      msH(data, data.subarray((hi - lo) / 2, hi), (hi - lo) / 2, hi);
+    }
+  }
+  public static int[] subarray(int start, int end) {
+    int[] ary = new ary[end - start];
+    for (int i = 0; i < ary.length; i++) {
+      ary[i] = ary[start];
+      start++;
+    }
+    return ary;
+  }
+}
