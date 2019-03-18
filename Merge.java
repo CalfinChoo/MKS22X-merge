f1bb0b6224be244e4c7507c09bc6373e16b785a7
Calvin Chu <cchu00@stuy.edu>
2019-03-18 11:49:41 -0400

    classwork

--- Merge.java
+++ Merge.java
@@ -3,10 +3,22 @@ public class Merge {
   public static void mergesort(int[] data){
     msH(data, data, 0, data.length);
   }
+  public static void msH(int[] data, int lo, int hi) {
+    int[] temp = new int[hi - lo + 1];
+    if (hi > lo) {
+      msH(data, lo, (hi - lo) / 2);
+      msH(data, (hi - lo) / 2 + 1, hi);
+    }
+    int count = 0;
+    for (int i = lo; i <= hi; i++, count++) {
+      temp[count] = data[i];
+    }
+
+  }
   public static void msH(int[] data, int[] temp, int lo, int hi) {
     if (hi > lo) {
-      msH(data, data.subarray(lo, (hi - lo) / 2), lo, (hi - lo) / 2);
-      msH(data, data.subarray((hi - lo) / 2, hi), (hi - lo) / 2, hi);
+      msH(data, temp, lo, (hi - lo) / 2);
+      msH(data, temp, (hi - lo) / 2, hi);
     }
   }
   public static int[] subarray(int start, int end) {
